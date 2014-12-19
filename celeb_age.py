93d9e78d9f1a8ae92835b99cd7b8da2c2e3be60e
Adam Deppe <adam.deppe@gmail.com>
2014-12-18 23:25:48 -0500

    Handle Hammurabi - BC birthyear

--- celeb_age.py
+++ celeb_age.py
@@ -1,5 +1,9 @@
 import requests
+import re
 from BeautifulSoup import BeautifulSoup
+
+class NoBirthdayException(Exception):
+    pass
 
 def getPersonInfo(person):
 
@@ -7,22 +11,56 @@ def getPersonInfo(person):
     print url
 
     r = requests.get(url)
-
     soup = BeautifulSoup(r.text)
-    for span in soup.findAll('span'):
-        # print span
-        if span.get('class') == 'bday':
-            bday = span.string
+
+    bday = getBDay(soup)
+    if not bday:
+        bday = getPersondataBDay(soup)
+
+    if bday is None:
+        raise NoBirthdayException
 
     print bday
-
-    year, month, day = tuple(int(x) for x in bday.split('-'))
-    return year, month, day
+    return bday
 
 def getNamePath(person):
     return "_".join([name.capitalize() for name in person.split()])
 
 
+def getBDay(soup):
+    bday = None
+    for span in soup.findAll('span'):
+
+        classAttr = span.get('class')
+        if classAttr ==  'bday':
+            bday = tuple(int(x) for x in span.string.split('-'))
+            break
+
+    return bday
+
+def getPersondataBDay(soup):
+    bday = None
+    for tr in soup.findAll('tr'):
+
+        tds = tr.findAll('td')
+
+        if len(tds) == 2:
+            if isDateOfBirthPersondata(tds[0]):
+                birthdateStr = tds[1].string
+
+                #handle the 'c. 1728 BC' case
+                result = re.match("c. ([0-9]+) (BC)?", birthdateStr)
+                if result:
+                    year, bc = result.groups()
+                    factor = -1 if bc else 1
+
+                    #bc is a negative year
+                    bday = (int(year) * factor, 0, 0)
+                    break
+    return bday
+
+def isDateOfBirthPersondata(td):
+    return td.get('class') == 'persondata-label' and td.string == 'Date of birth'
 
 if __name__ == "__main__":
     main()
--- celeb_age.t.py
+++ celeb_age.t.py
@@ -3,9 +3,14 @@ import celeb_age
 
 class TestCelebAge(unittest.TestCase):
     def testAges(self):
-        self.assertEqual((1974, 11, 15), celeb_age.getPersonInfo('chad kroeger'))
-        self.assertEqual((1935,  1,  8), celeb_age.getPersonInfo('elvis'))
-        self.assertEqual((1990,  4, 15), celeb_age.getPersonInfo('EMMA WATSON'))
+        age = celeb_age.getPersonInfo
+        self.assertEqual((1974, 11, 15), age('chad kroeger'))
+        self.assertEqual((1935,  1,  8), age('elvis'))
+        self.assertEqual((1990,  4, 15), age('EMMA WATSON'))
+        self.assertEqual((-1728, 0,  0), age('hammurabi'))
+
+
+        self.assertRaises(celeb_age.NoBirthdayException, celeb_age.getPersonInfo, 'tower of hanoi')
 
 if __name__ == '__main__':
     unittest.main()
