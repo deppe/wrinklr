235f5d06323b9dd3bdbff6d0d91e51c8f8845150
Adam Deppe <adam.deppe@gmail.com>
2014-12-22 23:22:47 -0500

    Partially handle the '1st century BC' case

--- celeb_age.py
+++ celeb_age.py
@@ -2,65 +2,146 @@ import requests
 import re
 from BeautifulSoup import BeautifulSoup
 
+BSoup = lambda x: BeautifulSoup(x, convertEntities=BeautifulSoup.HTML_ENTITIES)
+
 class NoBirthdayException(Exception):
     pass
 
-def getPersonInfo(person):
+def getBDay(person):
+    soup = getWikiHTML(person)
+    bdays = getAllBDays(soup)
 
+    if not bdays:
+        raise NoBirthdayException
+
+    print 'bdays:', bdays
+    return bdays[0]
+
+def getWikiHTML(person):
     url = 'https://en.wikipedia.org/wiki/' + getNamePath(person)
     print url
 
     r = requests.get(url)
-    soup = BeautifulSoup(r.text)
+    return BSoup(r.text)
 
-    bday = getBDay(soup)
-    if not bday:
-        bday = getPersondataBDay(soup)
+def getAllBDays(soup):
+    funcs = [
+        getBDayString,
+        getPersondataBDayString,
+        getBornString
+    ]
 
-    if bday is None:
-        raise NoBirthdayException
+    bdays = []
 
-    print bday
-    return bday
+    for func in funcs:
+        bday = func(soup)
+
+        if bday:
+            bdays.append(convertBDayStrToDate(bday))
+
+    return bdays
+
 
 def getNamePath(person):
     return "_".join([name.capitalize() for name in person.split()])
 
 
-def getBDay(soup):
-    bday = None
+def getBDayString(soup):
     for span in soup.findAll('span'):
-
         classAttr = span.get('class')
         if classAttr ==  'bday':
-            bday = tuple(int(x) for x in span.string.split('-'))
-            break
+            return unescapeHTML(span.string)
+            # bday = tuple(int(x) for x in span.string.split('-'))
+            # break
 
-    return bday
+    return None
 
-def getPersondataBDay(soup):
-    bday = None
+# def getBornRowString(soup):
+#     for th in soup.findAll('th'):
+#         classAttr = span.get('class')
+#         if classAttr ==  'bday':
+#             return unescapeHTML(span.string)
+#             # bday = tuple(int(x) for x in span.string.split('-'))
+#             # break
+
+#     return None
+
+def getBornString(soup):
     for tr in soup.findAll('tr'):
+        foundName = False
 
+        for content in tr.contents:
+            
+            if htmlNameIs(content, 'th') and content.string == 'Born':
+                foundName = True
+
+            if foundName and htmlNameIs(content, 'td') and content.string:
+                return unescapeHTML(content.string)
+
+            #return unescapeHTML(span.string)
+
+def getPersondataBDayString(soup):
+    for tr in soup.findAll('tr'):
         tds = tr.findAll('td')
+        if len(tds) == 2 and isDateOfBirthPersondata(tds[0]):
+            return unescapeHTML(tds[1].string)
+    return None
 
-        if len(tds) == 2:
-            if isDateOfBirthPersondata(tds[0]):
-                birthdateStr = tds[1].string
+def htmlNameIs(element, tag):
+    try:
+        return True if element.name == tag else False
+    except:
+        return False
 
-                #handle the 'c. 1728 BC' case
-                result = re.match("c. ([0-9]+) (BC)?", birthdateStr)
-                if result:
-                    year, bc = result.groups()
-                    factor = -1 if bc else 1
+def convertBDayStrToDate(bdayString):
+    print 'date string:', bdayString
 
-                    #bc is a negative year
-                    bday = (int(year) * factor, 0, 0)
-                    break
-    return bday
+    #'1987-09-09'
+    matches = re.search(r"(\d{4})-(\d{2})-(\d{2})", bdayString)
+    if matches:
+        return tuple(int(x) for x in matches.groups())
+    
+    #'c. 6210 BC'
+    matches = re.search(r"(c\.)?\s*([0-9]+)\s+(\bBC\b|\bAD\b)", bdayString)
+    if matches:
+        c, year, bc = matches.groups()
+        factor = -1 if bc.upper() == 'BC' else 1
+
+        #bc is a negative year
+        return (int(year) * factor, 0, 0)
+
+    #'January, 6210 BC'
+    matches = re.search(r"(\w+),\s+([0-9]+)\s+(\bBC\b|\bAD\b)", bdayString)
+    if matches:
+        month, year, bc = matches.groups()
+        factor = -1 if bc.upper() == 'BC' else 1
+
+        #bc is a negative year
+        return (int(year) * factor, 0, 0)
+
+    #1st century BC
+    matches = re.search(r"([0-9])+..\s+century\s+(\bBC\b|\bAD\b)", bdayString)
+    if matches:
+        century, bc = matches.groups()
+
+        factor = -1 if bc.upper() == 'BC' else 1
+
+        year = ((int(century) - 1) * 100 + 1) * factor
+
+        return (year, 0, 0)
+
+
+    return None
+
+def unescapeHTML(string):
+    return string.replace(u'\xa0', ' ')
 
 def isDateOfBirthPersondata(td):
     return td.get('class') == 'persondata-label' and td.string == 'Date of birth'
+
+
+
+    
 
 if __name__ == "__main__":
     main()
--- celeb_age.t.py
+++ celeb_age.t.py
@@ -1,16 +1,64 @@
 import unittest
 import celeb_age
+from BeautifulSoup import BeautifulSoup
+
 
 class TestCelebAge(unittest.TestCase):
     def testAges(self):
-        age = celeb_age.getPersonInfo
+        age = celeb_age.getBDay
         self.assertEqual((1974, 11, 15), age('chad kroeger'))
         self.assertEqual((1935,  1,  8), age('elvis'))
         self.assertEqual((1990,  4, 15), age('EMMA WATSON'))
         self.assertEqual((-1728, 0,  0), age('hammurabi'))
+        self.assertEqual((-69,   0,  0), age('Cleopatra'))
+        self.assertEqual((-1,   0,  0), age('jesus')) #1st century, 7-2 BC
 
 
-        self.assertRaises(celeb_age.NoBirthdayException, celeb_age.getPersonInfo, 'tower of hanoi')
+        self.assertRaises(celeb_age.NoBirthdayException, age, 'tower of hanoi')
+        self.assertRaises(celeb_age.NoBirthdayException, age, 'blahbloooooo')
+
+    def testParseBornHtml(self):
+        bs = celeb_age.BSoup
+        text = ('<tr>\n'
+                '<th scope="row" style="text-align:left">Born</th>\n'
+                '<td><span style="display:none">(<span class="bday">1974-11-15</span>)</span> November 15, 1974 <span class="noprint ForceAgeToShow">(age&#160;40)</span><br />\n'
+                '<a href="/wiki/Hanna,_Alberta" title="Hanna, Alberta">Hanna</a>, <a href="/wiki/Alberta" title="Alberta">Alberta</a>, <a href="/wiki/Canada" title="Canada">Canada</a></td>\n'
+                '</tr>\n')
+
+        self.assertEqual(celeb_age.getBDayString(bs(text)), '1974-11-15')
+
+        text = ('<tr>\n'
+                '<td class="persondata-label" style="color:#aaa;">Date of birth</td>\n'
+                '<td>November 15, 1974</td>\n'
+                '</tr>\n')
+
+        self.assertEqual(celeb_age.getPersondataBDayString(bs(text)), 'November 15, 1974')
+
+        text = ('</tr>\n' \
+                '<tr>\n' \
+                '<td class="persondata-label" style="color:#aaa;">Date of birth</td>\n' \
+                '<td>January, 69&#160;BC</td>\n' \
+                '</tr>\n')
+
+        self.assertEqual(celeb_age.getPersondataBDayString(bs(text)), 'January, 69 BC')
+
+
+        text = ('<tr>\n' \
+                '<th >Born</th>\n' \
+                '<td>7-2 BC</td>\n' \
+                '</tr>\n')
+
+        self.assertEqual(celeb_age.getBornString(bs(text)), '7-2 BC')
+
+    def testParseDateString(self):
+        parse = celeb_age.convertBDayStrToDate
+
+        self.assertEqual((1987, 9, 9), parse('1987-09-09'))
+        self.assertEqual((-49, 0, 0), parse('c. 49 BC'))
+        self.assertEqual((49, 0, 0), parse('49 AD'))
+        self.assertEqual((-100, 0, 0), parse('January, 100 BC'))
+        self.assertEqual((-201, 0, 0), parse('3rd century BC'))
+
 
 if __name__ == '__main__':
     unittest.main()
