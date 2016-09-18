4510b064e1d58c44e6fa4046fc9192d01cf3efef
Adam Deppe <adam.deppe@gmail.com>
2016-09-18 18:47:07 -0400

    Modify static files dirs

--- wrinklr_django/settings.py
+++ wrinklr_django/settings.py
@@ -78,11 +78,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
 )
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
