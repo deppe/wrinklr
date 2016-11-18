e2264d742fb22c769c86a51a72ff64cb88e83524
Adam Deppe <adam.deppe@gmail.com>
2016-11-17 23:48:00 -0500

    Add debug mode to startup

--- manage.py
+++ manage.py
@@ -2,9 +2,22 @@
 import os
 import sys
 
+from argparse import ArgumentParser
+
+from django.core.management import execute_from_command_line
+from django.conf import settings
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wrinklr_django.settings")
 
-    from django.core.management import execute_from_command_line
+    #Add -d argument for easily setting DEBUG = True
+    parser = ArgumentParser()
+    parser.add_argument('-d', '--debug', default=False, action='store_true')
+    args, unknown = parser.parse_known_args()
 
+    if args.debug:
+        os.environ.setdefault("DEBUG", 'True')
+
+    sys.argv[1:] = unknown
+    
     execute_from_command_line(sys.argv)
