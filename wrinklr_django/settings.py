8dac0cb3a0d0cf5ac9a49d74d01ba6d6ad942413
Adam Deppe <adam.deppe@gmail.com>
2016-11-19 20:19:29 -0500

    Add some security settings

--- wrinklr_django/settings.py
+++ wrinklr_django/settings.py
@@ -1,13 +1,3 @@
-"""
-Django settings for wrinklr_django project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import sys
@@ -17,9 +7,6 @@ import dj_database_url
 logger = logging.getLogger('wrinklr')
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -106,7 +93,16 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware'
 )
+
+# Security settings
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
 
 ROOT_URLCONF = 'wrinklr_django.urls'
 WSGI_APPLICATION = 'wrinklr_django.wsgi.application'
@@ -119,7 +115,6 @@ NOSE_ARGS = [
 ]
 
 # Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 if 'test' in sys.argv:
     # Test mode, force using a test database
     db = dj_database_url.parse('postgres://adeppe@localhost/testdb')
@@ -133,7 +128,6 @@ DATABASES = {
 logger.info('Using db %s as %s', db['NAME'], db['USER'])
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
@@ -142,7 +136,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
